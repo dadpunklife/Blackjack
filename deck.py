0ede9f8a1bf2ad9ec2244ade573292e5fe00d0d8
ATREMARC <atremarc@holycross.edu>
2020-08-02 16:50:03 -0400

    added deck and card classes with init function for building a deck of 52 cards

---
+++ deck.py
@@ -0,0 +1,22 @@
+# A class which represents a deck of 52 standard playing cards
+class Deck:
+
+    possible_values = [ 'A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K' ]
+    possible_suits = [ 'Hearts', 'Diamonds', 'Clubs', 'Spades' ]
+
+    cards = []
+
+    def __init__(self):
+        for value in self.possible_values:
+            for suit in self.possible_suits:
+                self.cards.append(Card(value, suit))
+        print("cards created: " + str(len(self.cards)))
+
+
+
+# A clas which represents a single card in a deck of standard playing cards
+class Card:
+
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
